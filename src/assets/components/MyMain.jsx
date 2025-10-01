84ba3013e98fb666ba9a225726740aea67d02170
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-01 17:18:06 +0200

    Aggiungi ricerca per Titoli e gestisci ricerca incrociata

--- src/assets/components/MyMain.jsx
+++ src/assets/components/MyMain.jsx
@@ -12,8 +12,10 @@ const MyMain = () => {
         { title: 'Pulp Fiction', genre: 'Thriller' }
     ]
 
-    // stato del campo di ricerca
-    const [search, setSearch] = useState("");
+    // stato del campo di ricerca del genere
+    const [searchGenre, setSearchGenre] = useState("");
+    // stato del campo di ricerca del titolo
+    const [searchTitle, setSearchTitle] = useState("");
     // stato del array filtrato
     const [filteredTasks, setFilteredTasks] = useState(films);
 
@@ -22,41 +24,69 @@ const MyMain = () => {
     useEffect(() => {
         setFilteredTasks(
             films.filter(film => {
-                console.log("film.genre:", film.genre)
-                console.log("search:", search)
-                // se lo stato del campo di ricerca non è nè vuoto nè "Qualsiasi" allora filtro i film in base al genere, altrimenti restituisco tutti i film
-                if (search !== "" && search !== "Qualsiasi") {
-                    return film.genre === search
+                // controllo lo stato dei campi di ricerca del genere e del titolo
+                const checkGenre = searchGenre !== "" && searchGenre !== "Qualsiasi";
+                const checkTitle = searchTitle !== "" && searchTitle !== "Qualsiasi";
+                // se sia genere che titolo hanno valori diversi da stringa vuota e da "Qualsiasi" allora devo effettuare un doppio controllo
+                if (checkGenre && checkTitle) {
+                    return film.genre === searchGenre && film.title === searchTitle
                 }
+                // se solo genere ha un valore valido, filtro per i generi dei film
+                else if (checkGenre && !checkTitle) {
+                    return film.genre === searchGenre;
+                }
+                // se solo titolo ha un valore valido, filtro per i titoli dei film
+                else if (!checkGenre && checkTitle) {
+                    return film.title === searchTitle;
+                }
+                // se entrambi non hanno valori validi, restituisco direttamente tutti i film
                 else {
-                    return film.genre;
+                    return film;
                 }
             })
         );
 
-    }, [search]);
+    }, [searchGenre, searchTitle]);
 
     return (
         <>
+            <div className="filtra">
+                <h2>GENERE</h2>
 
-            {/* <input type="text"
-                placeholder='Cerca'
-                value={search}
-                onChange={(e) => { setSearch(e.target.value) }}
-            /> */}
+                <select id="generi" name="scelta-genere"
+                    onChange={(e) => { setSearchGenre(e.target.value) }}>
 
-            <select id="generi" name="scelta-genere"
-                onChange={(e) => { setSearch(e.target.value) }}>
+                    <option value="Qualsiasi">Qualsiasi</option>
+                    <option value="Fantascienza">Fantascienza</option>
+                    <option value="Thriller">Thriller</option>
+                    <option value="Romantico">Romantico</option>
+                    <option value="Azione">Azione</option>
 
-                <option value="Qualsiasi">Qualsiasi</option>
-                <option value="Fantascienza">Fantascienza</option>
-                <option value="Thriller">Thriller</option>
-                <option value="Romantico">Romantico</option>
-                <option value="Azione">Azione</option>
+                </select>
 
-            </select>
+                <h2>TITOLO</h2>
+
+                <select id="generi" name="scelta-genere"
+                    onChange={(e) => { setSearchTitle(e.target.value) }}>
+
+                    <option value="Qualsiasi">Qualsiasi</option>
+                    <option value="Inception">Inception</option>
+                    <option value="Il Padrino">Il Padrino</option>
+                    <option value="Titanic">Titanic</option>
+                    <option value="Batman">Batman</option>
+                    <option value="Interstellar">Interstellar</option>
+                    <option value="Pulp Fiction">Pulp Fiction</option>
+
+                </select>
+            </div>
+
+
 
             <ul>
+                <li>
+                    <h2>TITOLO:</h2>
+                    <h3>GENERE:</h3>
+                </li>
                 {filteredTasks.map((film, index) => (
                     <li
                         key={index}>
